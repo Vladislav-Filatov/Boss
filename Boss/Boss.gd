caad87d3f114a4f6a512e373af91d73b807afa6e
kratium <mrgabzik@mail.ru>
2021-01-23 19:22:16 +0300

    1
    1

--- Boss/Boss.gd
+++ Boss/Boss.gd
@@ -4,13 +4,16 @@ extends Area2D
 #move
 #shooting
 #spawn
-
+var speed = 0
 
 func _ready():
-	var speed = Vector2()
 	var rand = RandomNumberGenerator.new()
 	rand.randomize()
 	speed = rand.randf_range(50, 400)
 
 func _physics_process(delta):
 	pass
+	
+func shoot():
+	pass 
+	
