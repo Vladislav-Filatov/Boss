dee95e805919b2ff9c9e492aa5be0b137d5664a7
Vladislav-Filatov <73752128+Vladislav-Filatov@users.noreply.github.com>
2021-01-27 21:57:27 +0300

    shoot
    Стрельба, которую делал в каи слетела, начал делать заново. Пока не доделал

--- Boss/Boss.gd
+++ Boss/Boss.gd
@@ -5,10 +5,17 @@ extends Area2D
 #shooting
 #spawn
 onready var timer= $teleportimer
+onready var firingPositions := $firingPositions
+onready var fireDelayTimer := $fireDelayTimer
+export var fireDelay := 0.1
+
+var BulletBoss:= preload("res://BulletBoss/BulletBoss.tscn")
+
 var vel = Vector2()
 var life = 250
 func _ready():
 	pass
+	
 func _physics_process(delta):
 	if timer.is_stopped():
 		var rand = RandomNumberGenerator.new()
@@ -18,12 +25,18 @@ func _physics_process(delta):
 		var rand1 = RandomNumberGenerator.new()
 		rand1.randomize()
 		vel.y = rand1.randi_range(50, 300)
-		timer.start(10)
+		timer.start(1)
 	position = vel
 	if life ==0:
 		kill()
+		
 func shoot():
-	pass 
+	if fireDelayTimer.is_stopped(): 
+		fireDelayTimer.start(fireDelay)
+		for child in firingPositions.get_children():
+			var bullet = BulletBoss.instance()
+			bullet.global_position = child.global_position
+			get_tree().current_scene.add_child(bullet)
 
 func kill():
 	queue_free()
